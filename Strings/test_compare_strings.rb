a2b1ad89aca146f6afcbb4955cff74df40029638
doverh <doverheld@gmail.com>
2017-01-26 22:15:44 -0500

    Adding final lotery files

--- Strings/test_compare_strings.rb
+++ Strings/test_compare_strings.rb
@@ -5,13 +5,12 @@ require_relative "compare_strings.rb"
 class TestCompareStrings < Minitest::Test
 
 	# Compare if two strings have at least 3 chars equals  
-	def assert_3_equal_chars
-		array=['1234','1224']
-		assert_equal(true,compare(array))
+	def test_3_equal_chars
+		assert_equal(true,compare('1234','1224'))
 	end
 
     # Compare if two strings have less then 3 chars equals  
-	def assert_less_than_3_equal_chars
+	def test_less_than_3_equal_chars
 		assert_equal(false,compare('1234','1325'))
 	end
 end
