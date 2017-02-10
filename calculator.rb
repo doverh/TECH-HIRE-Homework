1b646fdbe6fb55b72fe84bf89c5294524e12f1c4
doverh <doverheld@gmail.com>
2017-02-09 21:57:29 -0500

    Updating code after discussing with my group

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,57 @@
+def get_values(n)
+  #"n" number of times start loop with index i=0"
+  n.times.map.with_index { |i|
+    print "Enter value ##{ 1 + i }: "
+    gets.chomp.to_i 
+  }
+end
+
+def calculate()
+puts "Would you like to add : +, multiply: *, subtract - , divide /, exponent **?"
+
+while response = gets.chomp
+
+	case response.downcase
+	when 'add','+'
+	  puts "Which numbers would you like to add?"
+	  operator = :+
+	  break #make sure user input correct choice 
+	when 'subtract', '-'
+	  puts "Which numbers would you like to subtract?"
+	  operator = :-
+	  break
+	when 'multiply', '*'
+	  puts "Which numbers would you like to multiply?"
+	  operator = :*
+	  break
+	when 'divide', '/'
+	  puts "Which numbers would you like to divide?"
+	  operator = :/
+	  break
+	when 'exponent', '**'
+	  puts "Which numbers would you like to exponent?"
+	  operator = :**
+	  break
+	 
+ else
+	puts "Please, would you like to add : +, multiply: *, subtract - , divide /, exponent **?"
+ end
+
+end
+	puts "How many values?"
+num_of_values = gets.to_i
+
+answer = get_values(num_of_elements).inject(operator)
+puts "The answer is... #{ answer }"
+
+end
+
+
+
+# def addition(num1, num2)
+# 	return num1 + num2
+# end
+
 
 # def interface()
 # 	puts "Insert a number: "
@@ -17,54 +71,4 @@
 # 	return calculation(num1, operation, num2)
 
 # end
-def get_int_values(n)
-  n.times { |n|
-    print "Enter value ##{ 1 + n }: "
-    gets.chomp.to_i 
-  }
-end
-
-def calculate()
-puts "Would you like to add : +, multiply: *, subtract - , divide /, exponent **?"
-response = gets.chomp
-
-puts "How many values?"
-num_of_values = gets.to_i
-
-case response.downcase
-when 'add' || '+'
-  puts "Which numbers would you like to add?"
-  operator = :+
-
-when 'subtract'
-  puts "Which numbers would you like to subtract?"
-  operator = :-
-
-when 'multiply'
-  puts "Which numbers would you like to multiply?"
-  operator = :*
-
-when 'divide'
-  puts "Which numbers would you like to divide?"
-  operator = :/
-
-when 'exponent'
-  puts "Which numbers would you like to exponent?"
-  operator = :**
-end
-
-
-answer = get_int_values(num_of_values).inject(operator)
-puts "The answer is... #{ answer }"
-
-end
-
-
-calculate()
-# def addition(num1, num2)
-# 	return num1 + num2
-# end
-
-
-
 
--- test_calculator.rb
+++ test_calculator.rb
@@ -5,10 +5,6 @@ class TestCalculator < Minitest::Test
 
 
 def test_addition
-	puts "insert a number: "
-	num1 = gets.chomp.to_i
-	puts "insert another number: "
-	num2 = gets.chomp.to_i
-	assert_equal(16,addition(num1, num2))
+	assert_equal(15,get_values(3).inject(:+))
 end
 end	
